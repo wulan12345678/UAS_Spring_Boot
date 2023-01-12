19ae6bdc3d7d0a9e6ac85cf2092840bd99ee215a
wulan12345678 <101958309+wulan12345678@users.noreply.github.com>
2023-01-12 13:05:31 +0700

    Add files via upload

---
+++ Membuat-spring-boot-crud/data.sql
@@ -0,0 +1,2 @@
+insert into employee (id, first_name, last_name) values (1, 'WULAN', 'DARI');
+insert into employee (id, first_name, last_name) values (2, 'SISKA', 'YUNI');
---
+++ Membuat-spring-boot-crud/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into employee (id, first_name, last_name) values (1, 'wulan', 'dari');
+insert into employee (id, first_name, last_name) values (2 'sis', 'yuni');
